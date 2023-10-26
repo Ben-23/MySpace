037d18799af74634858c5737c85e981d5925c642
Ben-23 <ben.nicholas233@gmail.com>
2023-10-27 01:26:40 +0530

    displaying basic details in homepage

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,11 @@ builder.Services.AddDbContext<DataContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Model"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
--- clientApp/src/app/app.component.ts
+++ clientApp/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ClientApp';
+  users:any;
+  constructor(private http:HttpClient){}
+  
+  ngOnInit(): void {
+   
+    this.http.get("http://localhost:5114/api/user").subscribe({
+      next:response=> this.users=response,
+      error: error=>console.log(error),
+      complete: ()=>console.log("Success"),
+  });
+    
+  }
+
 }
