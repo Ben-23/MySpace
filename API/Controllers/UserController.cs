90686b5abdd654efb3d5f3b8066bf4269d1bf460
Ben-23 <ben.nicholas233@gmail.com>
2023-10-29 17:26:38 +0530

    register API endpoint

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using API.Data;
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTO;
 using API.Entity;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
@@ -27,9 +30,23 @@ public class UserController:ControllerBase
     {
         return await _context.Users.FindAsync(id);
     }
-    //[HttpPost("register")]
-    // public async Task<ActionResult<User>> Register()
-    // {
-        
-    // }
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(RegisterDTO registerDTO)
+    {
+        if(await ExistingUser(registerDTO.UserName))
+            return BadRequest("Username Exists");
+        var hmac=new HMACSHA512();
+        var user= new User{
+            UserName=registerDTO.UserName.ToLower(),
+            PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt=hmac.Key
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();        
+        return user;
+    }
+    public async Task<bool> ExistingUser(string userName)
+    {
+        return await _context.Users.AnyAsync(x=> x.UserName==userName.ToLower());
+    }
 }
---
+++ API/DTO/RegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO;
+
+public class RegisterDTO
+{
+        [Required]
+    	public string UserName { get; set; }
+        [Required]
+        public string  Password { get; set; }
+}
