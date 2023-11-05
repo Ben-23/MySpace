d43f3d51492e4522a2cf359311b2ece804e07ce0
Ben-23 <ben.nicholas233@gmail.com>
2023-11-06 03:00:46 +0530

    added auth configurations

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API;
 using API.Data;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,16 @@ builder.Services.AddDbContext<DataContext>(options=>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Model"));
 });
 builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options=>
+{
+    options.TokenValidationParameters=new TokenValidationParameters{
+        ValidateIssuerSigningKey=true,
+        IssuerSigningKey= new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Tokenkey"])),
+        ValidateIssuer=false,
+        ValidateAudience=false
+    };
+});
 builder.Services.AddCors();
 
 var app = builder.Build();
