3a16ee8a75ed7d9c890395363e59f10947e49811
Ben-23 <ben.nicholas233@gmail.com>
2024-01-29 22:19:25 +0530

    persistance wuth async pipe

--- clientApp/src/app/nav/nav.component.ts
+++ clientApp/src/app/nav/nav.component.ts
@@ -11,31 +11,21 @@ export class NavComponent implements OnInit {
   loggedIn = false;
   model: any = {};
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: Response => {
         console.log(Response);
-        this.loggedIn = true
       },
       error: error => console.log(error)
     })
   }
   logout() {
-    this.loggedIn = false;
     this.accountService.logout();
   }
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(
-      {
-        next: user => this.loggedIn = !!user,
-        error: error => console.log(error)
-      }
-    )
-  }
+
 }
